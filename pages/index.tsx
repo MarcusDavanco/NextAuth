2ed8010138478829dbd9260307b85c565fdfe291
Marcus Davanço <md.daves@gmail.com>
2022-12-28 19:30:42 -0300

    feat: Validando visitante

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react';
-import { parseCookies } from 'nookies'
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest'
 
 
 export default function Home() {
@@ -31,19 +31,10 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx)
+export const getServerSideProps = withSSRGuest(async (ctx) => {
 
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
 
   return {
     props: {}
   }
-}
+});
