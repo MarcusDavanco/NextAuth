21abfc793a701c955c11d2b17116cbf82afeb0c2
Marcus Davanço <md.daves@gmail.com>
2022-12-30 19:28:36 -0300

    Criando hook de permissão

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,18 @@
+import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
+import { AuthTokenError } from "../services/errors/AuthTokenError"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
+
+  const userCanSeeMetrics = useCan({
+    roles: ['administrator', 'editor']
+  })
 
   useEffect(() => {
     api.get('/me').then(response => console.log(response))
@@ -13,7 +20,10 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard: {user?.email} </h1>
+    <>
+      <h1>Dashboard: {user?.email} </h1>
+      {userCanSeeMetrics && <div>Métricas</div>}
+    </>
   )
 
 }
@@ -21,9 +31,6 @@ export default function Dashboard() {
 export const getServerSideProps = withSSRAuth(async (ctx) => {
   const apiClient = setupAPIClient(ctx)
   const response = await apiClient.get('/me');
-
-  console.log(response.data)
-
   return {
     props: {}
   }
